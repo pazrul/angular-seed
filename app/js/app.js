d25acf4e78bf2d258d00a4504d681b1314b8280a
Jeremy Whitbred <pazrul@gmail.com>
2012-12-28 00:39:19 -0800

    Start population of views

--- app/js/app.js
+++ app/js/app.js
@@ -4,8 +4,9 @@
 // Declare app level module which depends on filters, and services
 angular.module('tossersHonor', ['tossersHonor.filters', 'tossersHonor.services', 'tossersHonor.directives']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/cities', {templateUrl: 'partials/city.html', controller: MyCtrl1});
-    $routeProvider.when('/games', {templateUrl: 'partials/game-in-progress.html', controller: MyCtrl2});
-    $routeProvider.when('/players', {templateUrl: 'partials/partial2.html', controller: MyCtrl2});
-    $routeProvider.otherwise({redirectTo: '/view1'});
+    $routeProvider.when('/cities', {templateUrl: 'partials/city.html', controller: CitiesListCtrl});
+    $routeProvider.when('/games', {templateUrl: 'partials/game-in-progress.html', controller: CtrlMove});
+    $routeProvider.when('/players', {templateUrl: 'partials/players.html', controller: CtrlMove});
+    $routeProvider.when('/game', {templateUrl: 'partials/game.html', controller: CtrlMove});
+    $routeProvider.otherwise({redirectTo: '/cities'});
   }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,10 +3,24 @@
 /* Controllers */
 
 
-function MyCtrl1() {}
-MyCtrl1.$inject = [];
+function CtrlMove() {}
+CtrlMove.$inject = [];
 
 
 function MyCtrl2() {
 }
 MyCtrl2.$inject = [];
+
+function CitiesListCtrl($scope, $http) {
+	$http.get('cities/cities.json').success(function(data){
+		$scope.cities = data;
+	});
+}
+//CitiesListCtrl.$inject = ['$scope', '$http'];
+
+function CityDetailCtrl($scope, $routeParams, $http) {
+$http.get('cities/' + $routeParams.cityId + '.json').success(function(data) {
+  $scope.phone = data;
+});
+}
+//PhoneDetailCtrl.$inject = ['$scope', '$routeParams', '$http'];
